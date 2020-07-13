224d1e58f76527ce378539d36d42a31d6c1764f4
GabySantana <gsantana@frontrunnertechnologies.net>
2020-07-13 15:06:09 -0500

    add Details screen complete

--- src/components/Details.js
+++ src/components/Details.js
@@ -9,18 +9,16 @@ import AsyncStorage from "@react-native-community/async-storage";
 import {CartContext} from "../constant/Context";
 import LoadingScreen from "./LoadingScreen";
 
-
 const Details = ({route, navigation}) => {
-
+    var {item} = route.params;
     const [dataRelated, setdataRelated] = React.useState([]);
-    const [dataNewRel, setdataNewRel] = React.useState([]);
-    const [item1, setItem] = React.useState();
-    const [itemId, setItemId] = React.useState();
-    const [isLoaded, setIsLoaded] = React.useState(false);
+    //const [dataNewRel, setdataNewRel] = React.useState([]);
+    //const [item1, setItem] = React.useState();
+    //const [itemId, setItemId] = React.useState();
+    //const [isLoaded, setIsLoaded] = React.useState(false);
     const [shoppingCart, setShoppingCart] = React.useContext(CartContext);
-
-    const {item} = route.params;
-
+    const [element, setElement] = React.useState(item);
+    //const [flag, setFlag] = React.useState(false);
 
     const BackIcon = (props) => (
         <Icon {...props} name='arrow-back' />
@@ -32,185 +30,120 @@ const Details = ({route, navigation}) => {
     const regex = /(<([^>]+)>)/ig;
 
     const related = async () =>{
-            for (let i = 0; i < item.related_ids.length; i++) {
-
-                const oauth_consumer_key = Constants.CLIENT_KEY;
-                const oauth_nonce = Math.floor(Date.now() / 1000) + [i];
-                const oauth_signature_method = 'HMAC-SHA1'
-                let timeStamp = Math.floor(Date.now() / 1000);
-                let url = Constants.URL + Constants.GET_PRODUCTS + '/' + item.related_ids[i]
-                const concat = 'oauth_consumer_key=' + oauth_consumer_key + '&oauth_nonce=' + oauth_nonce + '&oauth_signature_method=' + oauth_signature_method + '&oauth_timestamp=' + timeStamp;
-                let base_string = 'GET&' + encodeURIComponent(url) + '&' + encodeURIComponent(concat)
-                let secret_key = Constants.CLIENT_SECRET;
-                var hmacsha1 = require('hmacsha1')
-                var oauth_signature = hmacsha1(secret_key + '&', base_string);
-                let testable_url = url + '?' + concat + '&oauth_signature=' + oauth_signature
-
-
-                await fetch(testable_url, {
-                    method: 'GET',
-                })
-                    .then((response) => response.json())
-                    .then((response) => {
+        let related = [];
+        for (let i = 0; i < element.related_ids.length; i++)
+        {
+            const oauth_consumer_key = Constants.CLIENT_KEY;
+            const oauth_nonce = Math.floor(Date.now() / 1000) + [i];
+            const oauth_signature_method = 'HMAC-SHA1'
+            let timeStamp = Math.floor(Date.now() / 1000);
+            let url = Constants.URL + Constants.GET_PRODUCTS + '/' + element.related_ids[i]
+            const concat = 'oauth_consumer_key=' + oauth_consumer_key + '&oauth_nonce=' + oauth_nonce + '&oauth_signature_method=' + oauth_signature_method + '&oauth_timestamp=' + timeStamp;
+            let base_string = 'GET&' + encodeURIComponent(url) + '&' + encodeURIComponent(concat)
+            let secret_key = Constants.CLIENT_SECRET;
+            var hmacsha1 = require('hmacsha1')
+            var oauth_signature = hmacsha1(secret_key + '&', base_string);
+            let testable_url = url + '?' + concat + '&oauth_signature=' + oauth_signature;
+            await fetch(testable_url, {
+                method: 'GET',
+            })
+                .then((response) => response.json())
+                .then((response) => {
+                    /*if(dataRelated.indexOf(response)===-1)
+                    {
                         setdataRelated(dataRelated => [...dataRelated, response]);
-                    })
-                    .catch((error) => {
-                        alert(error);
-                    });
-        }
-        if (item1.a.length > 0) {
-            setIsLoaded(isLoaded, true);
-            for (let j = 0; j < item1.a.length; j++) {
-
-                const oauth_consumer_key = Constants.CLIENT_KEY;
-                const oauth_nonce = Math.floor(Date.now() / 1000) + [j];
-                const oauth_signature_method = 'HMAC-SHA1'
-                let timeStamp = Math.floor(Date.now() / 1000);
-                let url = Constants.URL + Constants.GET_PRODUCTS + '/' + item.related_ids[j]
-                const concat = 'oauth_consumer_key=' + oauth_consumer_key + '&oauth_nonce=' + oauth_nonce + '&oauth_signature_method=' + oauth_signature_method + '&oauth_timestamp=' + timeStamp;
-                let base_string = 'GET&' + encodeURIComponent(url) + '&' + encodeURIComponent(concat)
-                let secret_key = Constants.CLIENT_SECRET;
-                var hmacsha1 = require('hmacsha1')
-                var oauth_signature = hmacsha1(secret_key + '&', base_string);
-                let testable_url = url + '?' + concat + '&oauth_signature=' + oauth_signature
-
-
-                await fetch(testable_url, {
-                    method: 'GET',
+                    }*/
+                    related.push(response);
+                    setdataRelated([...new Set(related)]);
                 })
-                    .then((response) => response.json())
-                    .then((response) => {
-                        // [dataNewRel, setdataNewRel]
-                        setdataNewRel(dataNewRel => [...dataNewRel, response]);
-                    })
-                    .catch((error) => {
-                        alert(error);
-                    });
-            }
-
+                .catch((error) => {
+                    alert(error);
+                });
         }
-    }
-    const newRel = [...new Set(dataRelated)];
-    const  relNewI = [...new Set(dataNewRel)];
-    // console.log(relNewI,'relNewItemsss deberia mostrar cosas')
-
+    };
 
     const addToCart = async (i) => {
         setShoppingCart([...shoppingCart,i]);
         await AsyncStorage.setItem('startShopping','add');
-        ToastAndroid.show("Product added successfully!", ToastAndroid.SHORT);
+    };
+
+    const changeElement = async (i) => {
+        setdataRelated([]);
+        setElement(i);
+        navigation.navigate('Details', { item: i, });
     };
 
     React.useEffect(()=>{
-
-        setItem(item1 => ({ ...item1, a: item.related_ids }), isLoaded, false);
-
         related();
-
     },[]);
+
+    React.useEffect(()=>{
+        related();
+    },[element]);
+
+    React.useEffect(()=>{
+        console.log('RELATED ' + dataRelated.length);
+    },[dataRelated]);
 
     return (
         <SafeAreaView >
-            <TopNavigation title={item.name} alignment='center' accessoryLeft={BackAction}/>
-            <Divider/>
+            <TopNavigation title={element.name} alignment='center' accessoryLeft={BackAction} />
+            <Divider />
             <Layout>
-
-            <ScrollView>
-                <View   >
-                    <View style={{width: '100%',height: '100%',backgroundColor: '#FFF',borderColor: '#bdbdbd',borderRadius: 5,borderWidth: 1,padding: 5,}}>
-                        <View style={{alignContent: 'center',alignItems: 'center',marginTop: 5,}} >
-                            <Image source={{uri: item.images[0].src}} style={styles.image}/>
-                        </View>
-                        <Text style={{textAlign: 'left', fontSize:20}}  >{item.name}</Text>
-
-                        <Text style={{textAlign: 'left', fontSize:20}} category="s2">
-                            $ {isNaN(Number.parseFloat(item.price).toFixed(2)) === false ? Number.parseFloat(item.price).toFixed(2) : Number.parseFloat(0).toFixed(2)}
+                <ScrollView>
+                    <View>
+                        <View style={{ackgroundColor: '#FFF', borderColor: '#bdbdbd', borderRadius: 5, borderWidth: 1, padding: 5, }}>
+                            <View style={{ alignContent: 'center', alignItems: 'center', marginTop: 5, }} >
+                                <Image source={{uri: element.images.length > 0 ? element.images[0].src : 'https://upload.wikimedia.org/wikipedia/commons/0/0a/No-image-available.png'}} style={styles.image} />
+                            </View>
+                            <Text style={{ textAlign: 'left', fontSize: 20 }}  >{element.name}</Text>
+                            <Text style={{ textAlign: 'left', fontSize: 20 }} category="s2">
+                                $ {isNaN(Number.parseFloat(element.price).toFixed(2)) === false ? Number.parseFloat(item.price).toFixed(2) : Number.parseFloat(0).toFixed(2)}
                             </Text>
-                        <Text style={{textAlign: 'center'}} category="s2">&nbsp;</Text>
-                        <Button
-                        status="primary"
-                        appearance="outline"
-                        size="medium"
-                        onPress={() => addToCart(item)}>
-                        Add to cart
-                    </Button>
-                        <Text style={{paddingTop:35}}>AVAILABILITY: {item.stock_quantity} in stock</Text>
-                        <Text style={{fontSize: 20, paddingTop:25}}>Description</Text>
-                        <Text style={{paddingTop:5}}>{item.short_description.replace(regex, '')}</Text>
-                        <Text style={{paddingTop:25, fontSize:30}}>Related Products</Text>
-
-
-                        {isLoaded  ?
-                            <Layout style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-                                <Spinner status='info' size='giant'/>
-                            </Layout>
-                            : (
-                            // {newRel.length > 0 ? (
-                                    <FlatList
-                                        data={relNewI.length > 0 ? (relNewI) : (newRel)}
-                                        numColumns={2}
-                                        contentContainerStyle={{paddingBottom: '50%'}}
-                                        renderItem={({item}) => (
-                                            <View style={{flex: 1, flexDirection: 'row', padding: 5}}>
-
-                                                <View onPress={() => {
-                                                    navigation.navigate('Details', {
-                                                        item: item.related_ids,})}}
-                                                      style={{width: '100%',height: 250,backgroundColor: '#FFF',borderColor: '#bdbdbd',borderRadius: 5,borderWidth: 1,padding: 5,}}>
-                                                    <View
-                                                        style={{alignContent: 'center',alignItems: 'center',marginTop: 5,}}>
-                                                        <Image
-                                                            source={{uri: item.images[0].src}}
-                                                            style={styles.imageR}
-                                                        />
-                                                    </View>
-                                                    <Text
-                                                        onPress={() => {
-                                                            navigation.navigate('Details', {
-                                                                item: item,})
-                                                            setItemId(item.id)
-                                                            // setItem(item1 => [item1, item.related_ids])
-                                                            // setItemId(itemId =>({...itemId, a: item.id}));
-
-                                                            setIsLoaded(true)
-                                                            // relatesd(item)
-                                                            related(item)
-                                                        }}
-                                                        style={{textAlign: 'center'}} category="h6" >
-                                                        {item.name}
-                                                        {item.id}
-                                                    </Text>
-                                                    <Text style={{textAlign: 'center'}} category="s2">
-                                                        $ {isNaN(Number.parseFloat(item.price).toFixed(2)) === false ? Number.parseFloat(item.price).toFixed(2) : Number.parseFloat(0).toFixed(2)}
-                                                    </Text>
-                                                    <Text style={{textAlign: 'center'}} category="s2">
-                                                        &nbsp;
-                                                    </Text>
-                                                    <Button
-                                                        status="primary"
-                                                        appearance="outline"
-                                                        // accessoryLeft={cartIcon}
-                                                        size="medium"
-                                                        onPress={() => addToCart(item)}>
-                                                        Add to cart
-                                                    </Button>
-                                                </View>
-                                            </View>
-                                        )}
-                                        keyExtractor={(item) => String(item.id)}
-                                    />
-
-                        // ) : (
-                        //     <Layout style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-                        //         <Spinner status='info' size='giant'/>
-                        //     </Layout>
-                        )}
+                            <Text style={{ textAlign: 'center' }} category="s2">&nbsp;</Text>
+                            <Button status='primary' appearance='outline' size='medium' onPress={() => addToCart(element)}>
+                                Add to cart
+                            </Button>
+                            <Text style={{ paddingTop: 35 }}>AVAILABILITY: {element.stock_quantity} in stock</Text>
+                            <Text style={{ fontSize: 20, paddingTop: 25 }}>Description</Text>
+                            <Text style={{ paddingTop: 5 }}>{element.short_description.replace(regex, '')}</Text>
+                            <Text style={{ paddingTop: 25, fontSize: 30 }}>Related Products</Text>
+                        </View>
                     </View>
-                </View>
-            </ScrollView>
+                    <View>
+                        <FlatList
+                            data={dataRelated.length > 0 ? dataRelated : []}
+                            numColumns={2}
+                            contentContainerStyle={{ paddingBottom: 50 }}
+                            renderItem={({ item }) => (
+                                <View style={{ flex: 1, flexDirection: 'row', padding: 5 }}>
+                                    <View onPress={() => { navigation.navigate('Details', { item: item }) }} style={{ width: '100%', height: 250, backgroundColor: '#FFF', borderColor: '#bdbdbd', borderRadius: 5, borderWidth: 1, padding: 5, }}>
+                                        <View style={{ alignContent: 'center', alignItems: 'center', marginTop: 5, }}>
+                                            <Image source={{uri: item.images != null ? item.images[0].src : 'https://upload.wikimedia.org/wikipedia/commons/0/0a/No-image-available.png'}} style={styles.imageR} />
+                                        </View>
+                                        <Text onPress={() => {changeElement(item)}}
+                                              style={{ textAlign: 'center' }} category="h6" >
+                                            {item.name}
+                                            {item.id}
+                                        </Text>
+                                        <Text style={{ textAlign: 'center' }} category="s2">
+                                            $ {isNaN(Number.parseFloat(item.price).toFixed(2)) === false ? Number.parseFloat(item.price).toFixed(2) : Number.parseFloat(0).toFixed(2)}
+                                        </Text>
+                                        <Text style={{ textAlign: 'center' }} category="s2">&nbsp;</Text>
+                                        <Button status="primary" appearance="outline" size="medium" onPress={() => addToCart(item)}>
+                                            Add to cart
+                                        </Button>
+                                    </View>
+                                </View>
+                            )}
+                            keyExtractor={(item) => String(item.id)}
+                        />
+                    </View>
+                </ScrollView>
             </Layout>
         </SafeAreaView>
-)}
+    )
+}
 const Stack = createStackNavigator();
 const styles = StyleSheet.create({
     imageR: {
